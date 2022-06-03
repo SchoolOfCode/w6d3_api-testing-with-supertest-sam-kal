6fa64b5b71c4d18a72901d48072ccd6b7ce9fb9a
Samantha Wu <samanthayywu@gmail.com>
2022-06-03 13:36:53 +0100

    get all users test

--- task-3/routes/users.test.js
+++ task-3/routes/users.test.js
@@ -1,9 +1,24 @@
 import app from "../app.js";
 import request from "supertest";
-import { describe, test, expect } from "/globals";
+import { describe, test, expect } from "@jest/globals";
 
 // GET /users
-
+describe(`get users`, () => {
+  test(`get all users`, async () => {
+    const res = await request(app).get("/users");
+    expect(res.statusCode).toBe(200);
+  });
+  test(`checks if response body is {
+    success: true,
+    payload: users,
+  }`, async () => {
+    const res = await request(app).get("/users");
+    expect(res.body).toEqual({
+      success: true,
+      payload: expect.any(Array),
+    });
+  });
+});
 // GET /users?username=some_username
 
 // GET /users/:id
--- task-3/server.js
+++ task-3/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-const port = 5010;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
